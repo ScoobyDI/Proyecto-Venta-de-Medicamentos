4e662a0c44f6343713cb77fa9133d3ef06d35062
B3lm0nd <renzojesuslopezjimenez75@gmail.com>
2024-09-29 20:05:22 -0500

    implementacion usuariobean

---
+++ model/UsuarioBean.php
@@ -0,0 +1,9 @@
+<?php
+class UsuarioBean {
+    public $id;                                     //creo una atributo id publico
+    public $correo;                                 //creo una atributo correo publico
+    public $contraseña;                             //creo una atributo contraseña publico
+    public $nombresUsuario;
+    public $apellidosUsuario;
+}
+?>
